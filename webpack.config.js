9ab03dcbf66bad7e26eadf156de5d3d3bd853347
Edgar <ecarrillo046@gmail.com>
2021-10-13 09:09:12 -0700

    add starter files and folders

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Form Collection',
+      template: 'src/index.html'
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  devtool: "source-map",
+};
